59810002967b292658b7c2993534ab0c0c0edc14
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2025-09-17 12:28:04 +0200

    fix images so they work both in md and frontmatter

--- src/utils/load-content-image.ts
+++ src/utils/load-content-image.ts
@@ -1,11 +1,11 @@
 import { type ImageMetadata } from "astro"
-
 const images = import.meta.glob<{
   default: ImageMetadata
 }>("/src/content/images/*.{jpeg,jpg,png,gif,webp}")
 
-export default async function (path?: string) {
-  if (!path) throw new Error("Cannot load content image for empty path")
+export default async function (aliasedPath?: string) {
+  if (!aliasedPath) throw new Error("Cannot load content image for empty path")
+  const path = aliasedPath.replace("$images", "/src/content/images")
   const loader = images[path]
   if (!loader) throw new Error(`content image not found: ${path}`)
   const image = await loader()
