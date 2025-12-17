18bb47e28c3700bc6f1b237d0ea412a4e00eba4c
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2025-12-17 20:05:07 +0100

    fix links to work for new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   site: "https://friaochantagnamurare.se",
-  base: "/",
   image: {
     responsiveStyles: true,
     layout: "constrained",
