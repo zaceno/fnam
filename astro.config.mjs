cb079292fe38a0d26b0a44a7be7ae212b189b0f5
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2025-12-17 19:20:18 +0100

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zaceno.github.io",
-  base: "fnam",
+  site: "https://friaochantagnamurare.se",
+  base: "/",
   image: {
     responsiveStyles: true,
     layout: "constrained",
