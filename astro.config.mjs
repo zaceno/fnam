ba52762d2fae3ec168d49eab79d61090246af35a
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2025-12-18 23:43:12 +0100

    update for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://friaochantagnamurare.se",
+
   image: {
     responsiveStyles: true,
     layout: "constrained",
   },
+
+  integrations: [sitemap()],
 })
